c24d6900e6710fad3bd712b4bd5609fdd7693c18
roeeaharoni <roee.aharoni@gmail.com>
2017-05-07 16:06:05 +0300

    added gpu example

--- src/example-gpu.sh
+++ src/example-gpu.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #install_name_tool -change libboost_program_options.dylib /Users/roeeaharoni/boost_1_55_0/stage/lib/libboost_program_options.dylib /Users/roeeaharoni/git/dynet/build/dynet/libdynet.dylib
 #install_name_tool -change libboost_serialization.dylib /Users/roeeaharoni/boost_1_55_0/stage/lib/libboost_serialization.dylib /Users/roeeaharoni/git/dynet/build/dynet/libdynet.dylib
-#base_path=/home/nlp/aharonr6
-base_path=/Users/roeeaharoni
+base_path=/home/nlp/aharonr6
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-python dynet-seq2seq-attn.py --dynet-mem 8000 --input-dim=100 --hidden-dim=100 --epochs=100 --lstm-layers=1 \
---optimization=ADADELTA --batch-size=1 --beam-size=10 --plot \
+
+python dynet-seq2seq-attn.py --dynet-mem 8000 --dynet-gpu-ids 3 --input-dim=100 --hidden-dim=500 --epochs=100 --lstm-layers=1 \
+--optimization=ADADELTA --batch-size=32 --beam-size=10 \
 $base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.de \
 $base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.en \
 $base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.de \
@@ -16,7 +16,7 @@ $base_path/git/research/nmt/data/news-de-en/test/newstest2016-deen.tok.penntrg.c
 $base_path/git/research/nmt/data/news-de-en/test/newstest2016-deen.tok.penntrg.clean.true.bpe.en \
 $base_path/git/dynet-seq2seq-attn/results/test4
 
-# train files
+# large train files
 #$base_path/git/research/nmt/data/news-de-en-50/train/news-commentary-v8.de-en.tok.penntrg.clean.true.bpe.de \
 #$base_path/git/research/nmt/data/news-de-en-50/train/news-commentary-v8.de-en.tok.penntrg.clean.true.bpe.en \
 
