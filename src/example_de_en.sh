528f524b3a8c3b11360f3f0f296c1a64be2f5a23
roeeaharoni <roee.aharoni@gmail.com>
2017-05-14 20:21:02 +0300

    added mt toy training scripts

---
+++ src/example_de_en.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#install_name_tool -change libboost_program_options.dylib /Users/roeeaharoni/boost_1_55_0/stage/lib/libboost_program_options.dylib /Users/roeeaharoni/git/dynet/build/dynet/libdynet.dylib
+#install_name_tool -change libboost_serialization.dylib /Users/roeeaharoni/boost_1_55_0/stage/lib/libboost_serialization.dylib /Users/roeeaharoni/git/dynet/build/dynet/libdynet.dylib
+base_path=/home/nlp/aharonr6
+#base_path=/Users/roeeaharoni
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+python dynet-seq2seq-attn.py --dynet-mem 4000 --input-dim=200 --hidden-dim=500 --epochs=100 --lstm-layers=1 \
+--optimization=ADADELTA --batch-size=1 --beam-size=10 --plot --eval-after=5000 \
+$base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.de \
+$base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.en \
+$base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.de \
+$base_path/git/research/nmt/data/news-de-en/dev/newstest2015-deen.tok.penntrg.clean.true.bpe.en \
+$base_path/git/research/nmt/data/news-de-en/test/newstest2016-deen.tok.penntrg.clean.true.bpe.de \
+$base_path/git/research/nmt/data/news-de-en/test/newstest2016-deen.tok.penntrg.clean.true.bpe.en \
+$base_path/git/dynet-seq2seq-attn/results/test_de_en_cpu
+
+
