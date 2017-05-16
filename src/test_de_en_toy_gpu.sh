7f4e7cc0b1e80a605fdaecc1876d13746735b2b7
roeeaharoni <roee.aharoni@gmail.com>
2017-05-16 11:46:27 +0300

    added toy examples dev

--- src/test_de_en_toy_gpu.sh
+++ src/test_de_en_toy_gpu.sh
@@ -10,10 +10,10 @@ python dynet-seq2seq-attn.py --dynet-gpu-ids 1 --dynet-mem 4000 --input-dim=200 
 --optimization=ADADELTA --batch-size=1 --beam-size=10 --plot --eval-after=10000 \
 $base_path//git/dynet-seq2seq-attn/data/toy_de.txt \
 $base_path/git/dynet-seq2seq-attn/data/toy_en.txt \
-$base_path//git/dynet-seq2seq-attn/data/toy_de.txt \
-$base_path/git/dynet-seq2seq-attn/data/toy_en.txt \
-$base_path//git/dynet-seq2seq-attn/data/toy_de.txt \
-$base_path/git/dynet-seq2seq-attn/data/toy_en.txt \
+$base_path//git/dynet-seq2seq-attn/data/toy_de_20.txt \
+$base_path/git/dynet-seq2seq-attn/data/toy_en_20.txt \
+$base_path//git/dynet-seq2seq-attn/data/toy_de_20.txt \
+$base_path/git/dynet-seq2seq-attn/data/toy_en_20.txt \
 $base_path/git/dynet-seq2seq-attn/results/test_de_en_toy_gpu
 
 # large train files
