bfe3473294d1b2acf9482d98b467ac5c7684b5c7
roeeaharoni <roee.aharoni@gmail.com>
2017-06-03 17:29:41 +0300

    removed unneeded prints

--- src/dynet-seq2seq-attn.py
+++ src/dynet-seq2seq-attn.py
@@ -944,8 +944,11 @@ def predict_multiple_sequences(params, x2int, y2int, int2y, inputs):
             # best hypothesis, sequence without probability
             predicted_seq = nbest[0][0]
 
-            for seq in nbest:
-                print ' '.join(seq[0]) + '\n'
+
+            # TODO: remove
+            print 'input: {}'.format(' '.join(input_seq))
+            for k, seq in enumerate(nbest):
+                print '{}-best: {}'.format(k, ' '.join(seq[0]) + '\n')
         else:
             predicted_seq, alphas_mtx = predict_output_sequence(params, input_seq, x2int, y2int, int2y)
         if i % 100 == 0 and i > 0:
