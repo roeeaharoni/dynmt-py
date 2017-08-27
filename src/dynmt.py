b5acaa8c2d639d19bb7cf4fa33c12edbea583aac
roeeaharoni <roee.aharoni@gmail.com>
2017-08-27 13:57:45 +0300

    debugging input masking

--- src/dynmt.py
+++ src/dynmt.py
@@ -846,11 +846,11 @@ def attend(blstm_outputs, h_t, w_c, v_a, w_a, u_a, input_masks = None):
         blstm_outputs = [blstm_outputs[-1]]
 
     # iterate through input states to compute attention scores
-    # TODO: mask (zero) scores for inputs that does not exist
+    # TODO: mask (zero-out) attention scores for padded inputs
     # scores = [v_a * dn.tanh(w_a * h_t + u_a * h_input) for h_input in blstm_outputs]
     w_a_h_t = w_a * h_t
     scores = [v_a * dn.tanh(dn.affine_transform([w_a_h_t, u_a, h_input])) for h_input in blstm_outputs]
-    print scores.npvalue().shape
+    print [s.npvalue().shape for s in scores]
     print len(input_masks)
 
     # normalize scores using softmax
