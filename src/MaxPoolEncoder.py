8a4c815f5d24777679e5fa76d6f3cb81247d13ad
roeeaharoni <roee.aharoni@gmail.com>
2017-09-01 11:57:39 +0300

    refactored plotting function added maxpool example

--- src/MaxPoolEncoder.py
+++ src/MaxPoolEncoder.py
@@ -11,12 +11,16 @@ class MaxPoolEncoder:
         encoded_inputs, masks = self.bilstm.encode_batch(input_seq_batch)
         max_output = dn.emax(encoded_inputs)
         max_masks = [[1]] * len(input_seq_batch)
+        print 'len masks'
+        print len(masks)
         print 'masks'
         print masks
+        print 'len max_masks'
+        print len(max_masks)
         print 'max_masks'
         print max_masks
         print 'encoded_inputs[0].dim'
-        print encoded_inputs[0].dim
+        print encoded_inputs[0].dim()
         print 'max_output.dim'
-        print max_output.dim
+        print max_output.dim()
         return [max_output], max_masks
