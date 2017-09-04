2a562e3763116d942e34675a41ad782af9f60c07
roeeaharoni <roee.aharoni@gmail.com>
2017-09-04 09:50:19 +0300

    max pooling visualization

--- src/MaxPoolEncoder.py
+++ src/MaxPoolEncoder.py
@@ -1,5 +1,7 @@
 import dynet as dn
 import BiLSTMEncoder
+from collections import defaultdict
+import math
 
 class MaxPoolEncoder:
     def __init__(self, x2int, params):
@@ -13,16 +15,35 @@ class MaxPoolEncoder:
 
         # one mask per step, all are [1]'s since only one state from max pool encoder
         max_masks = [[1] * len(input_seq_batch)]
-        # print 'len masks'
-        # print len(masks)
-        # print 'masks'
-        # print masks
-        # print 'len max_masks'
-        # print len(max_masks)
-        # print 'max_masks'
-        # print max_masks
-        # print 'encoded_inputs[0].dim'
-        # print encoded_inputs[0].dim()
-        # print 'max_output.dim'
-        # print max_output.dim()
+
+        # vizualize how many entries are selected from each input
+        self.values_per_input(encoded_inputs, input_seq_batch, max_output)
+
         return [max_output], max_masks
+
+    def values_per_input(self, encoded_inputs, input_seq_batch, max_output):
+        max = max_output.value()
+        vals = []
+        hist = defaultdict(int)
+        for i, input in enumerate(encoded_inputs):
+            v = input.value()
+            vals.append(v)
+        for i, max_val in enumerate(max):
+            for j, vec in enumerate(vals):
+                if math.fabs(max_val - vec[i]) < 0.000000000000001:
+                    hist[j] += 1
+        print '\n'
+        print 'seq:' + str(input_seq_batch[0])
+        print 'selected in position: '
+        for key in hist:
+            char = ''
+            if key == 0:
+                char = '<s>'
+            if key == len(input_seq_batch[0]) + 1:
+                char = '</s>'
+            if char == '':
+                char = input_seq_batch[0][key - 1]
+            print str(key) + '    ' + str(hist[key]) + '   ' + char
+        print sum(hist.values())
+        print 'seq len: ' + str(len(input_seq_batch[0]) + 2)
+        print '\n'
